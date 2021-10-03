051cd19c2929b3b6f21b711871ee6f023a5a231a
mjgutierre <68908889+mjgutierre@users.noreply.github.com>
2021-10-03 18:09:33 -0500

    Update usuario.py

--- usuario.py
+++ usuario.py
@@ -1,5 +1,5 @@
 from account import Account
 
 class usuario(Account):
-    def __init__(self, id, nombre, documento, contrasena, email):
-       super().__init__(id, nombre, documento, contrasena, email);
+    def __init__(self, id,ubicacionTiempoReal, nombre, contrasena, email, direccion, celular, usuario):
+       super().__init__(id, ubicacionTiempoReal, nombre, contrasena, email, direccion, celular, usuario);
