69f5ae19361f212a1fe4c7c854101ce3cbbe0868
mjgutierre <68908889+mjgutierre@users.noreply.github.com>
2021-10-03 18:14:43 -0500

    Create conductor.py

---
+++ conductor.py
@@ -0,0 +1,8 @@
+from account import Account
+
+class Conductor(Account):
+    licencia = str;
+
+    def __init__(self, id, ubicacionTiempoReal, nombre, contrasena, email, direccion, celular, usuario):
+       super().__init__(id, ubicacionTiempoReal, nombre, contrasena, email, direccion, celular, usuario);
+       self.licencia = licencia;
