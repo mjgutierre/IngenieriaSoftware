0bdae2bac0662b491198941a4832546e75b53cb1
mjgutierre <68908889+mjgutierre@users.noreply.github.com>
2021-10-03 18:08:24 -0500

    Update account.py

--- account.py
+++ account.py
@@ -1,13 +1,19 @@
 class Account:
     id = int;
+    ubicacionTiempoReal= str;
     nombre = str;
     contrasena = str;
-    documento = str;
     email = str;
+    direccion = str;
+    celular = int;
+    usuario = str; 
 
-    def __init__(self, id, nombre, documento, contrasena, email):
-        self.name = nombre;
-        self.document = documento;
-        self.password = contrasena;
+    def __init__(self, id,ubicacionTiempoReal, nombre, direccion, contrasena, email, celular, usuario):
+        self.ubicacionTiempoReal=ubicacionTiempoReal;
+        self.nombre = nombre;
+        self.cireccion = direccion;
+        self.contrasena = contrasena;
         self.email = email;
         self.id = id;
+        self.celular = celular;
+        self.usuario = usuario;
